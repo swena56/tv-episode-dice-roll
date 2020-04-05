2629a122234517274eb16d145a50ca9e8253ef78
Andrew Swenson <andrew.swenson@bestbuy.com>
2020-04-05 14:52:46 -0500

    re-order dependences

--- api.js
+++ api.js
@@ -5,6 +5,8 @@ const fetch = require('node-fetch');
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
 
+const base = `api_key${process.env.API_KEY}=&language=en-US`;
+
 express()
   .use(logger('dev'))
   .use(express.json())
@@ -13,13 +15,13 @@ express()
   .use(cors())
   .get('/search', (req, res) => {
     const query = encodeURI('law and order');
-    fetch(`https://api.themoviedb.org/3/search/tv?api_key=${process.env.API_KEY}&language=en-US&page=1&query=${query}&include_adult=false`)
+    fetch(`https://api.themoviedb.org/3/search/tv?${base}&page=1&query=${query}`)
       .then(response => response.json())
       .then(data => res.send(JSON.stringify(data)) )
   })
-  .get('/random', (req, res) => {
-    const query = encodeURI('law and order');
-    fetch(`https://api.themoviedb.org/3/search/tv?api_key=${process.env.API_KEY}&language=en-US&page=1&query=${query}&include_adult=false`)
+  .get('/about', (req, res) => {
+    const showId = 549;
+    fetch(`https://api.themoviedb.org/3/tv/${showId}?${base}`)
       .then(response => response.json())
       .then(data => res.send(JSON.stringify(data)) )
   })
